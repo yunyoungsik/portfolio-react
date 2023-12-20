ac98b68b934c5b4c6922976b0939fb0f5483b111
yunyoungsik <144635640+yunyoungsik@users.noreply.github.com>
2023-12-21 00:12:12 +0900

    20231221

--- src/pages/TrendDevice.jsx
+++ src/pages/TrendDevice.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
@@ -20,31 +20,6 @@ const TrendDevice = () => {
     }
 `;
 gsap.registerPlugin(ScrollTrigger);
-
-// const sectionRef = useRef(null); // useRef로 참조할 요소
-//   const triggerRef = useRef(null);
-
-//    // horizontal 스크롤 애니메이션
-//    useEffect(() => {
-//     const pin = gsap.fromTo( // from, to, fromTo가 있다.
-//       sectionRef.current, // Gsap 애니메이션이 시작되는 요소 위치
-//       { translateX: 0 }, // from 부분(초기 시작)
-//       {
-//         translateX: -500, // to 부분
-//         ease: "none", // 쓸데없는 애니메이션 없애는 부분
-//         ScrollTrigger: { // 스크롤 애니메이션 발생하는 부분
-//           trigger: triggerRef.current, // 스크롤이 발생되는 요소 위치
-//           start: "top top", // "요소위치 시작위치"
-//           end: "bottom center", // "요소위치 끝위치"
-//           scrub: 0.7, // 되감기 기능, 또한 스크롤을 부드러운 애니메이션 추가.
-//           pin: ".project-section", // 가로스크롤시 페이지를 고정할 수 있는 기능
-//         },
-//       }
-//     );
-//     return () => {
-//       pin.kill(); // 모든 애니메이션 중단
-//     };
-//   }, []);
 
 // useEffect(() => {
 //     window.onload = function() {
