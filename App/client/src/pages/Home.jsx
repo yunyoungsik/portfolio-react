f0bbdd21da90d7230345dc3c2c09aec6e1997011
yunyoungsik <yunyoungsik91@gmail.com>
2024-01-09 14:26:45 +0900

    20240109

--- App/client/src/pages/Home.jsx
+++ App/client/src/pages/Home.jsx
@@ -44,18 +44,10 @@ const Home = () => {
                                 <rect width="352" height="32" x="80" y="384" fill="var(--ci-primary-color, #000000)" className="ci-primary" />
                             </svg>
                         ) : (
-                            <svg viewBox="0 -0.5 21 21" version="1.1" fill="#000000">
-                                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                                <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
-                                <g id="SVGRepo_iconCarrier">
-                                    <defs> </defs>
-                                    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-                                        <g id="Dribbble-Light-Preview" transform="translate(-419.000000, -240.000000)" fill="#000000">
-                                            <g id="icons" transform="translate(56.000000, 160.000000)">
-                                                <polygon id="close-[#1511]" points="375.0183 90 384 98.554 382.48065 100 373.5 91.446 364.5183 100 363 98.554 371.98065 90 363 81.446 364.5183 80 373.5 88.554 382.48065 80 384 81.446"></polygon>
-                                            </g>
-                                        </g>
-                                    </g>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" fill="#000000">
+                                <path d="M0 0h48v48H0z" fill="none" />
+                                <g id="Shopicon">
+                                    <polygon points="40,5.172 24,21.172 8,5.172 5.171,8 21.172,24 5.171,40 8,42.828 24,26.828 40,42.828 42.828,40 26.828,24 42.828,8" />
                                 </g>
                             </svg>
                         )}
