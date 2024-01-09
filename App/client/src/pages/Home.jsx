69ebdf98ac99ee1048593c128fd68bb4d630395a
yunyoungsik <yunyoungsik91@gmail.com>
2024-01-09 14:21:16 +0900

    20240109

--- App/client/src/pages/Home.jsx
+++ App/client/src/pages/Home.jsx
@@ -11,7 +11,7 @@ import { homeAbout } from '../assets/script/homeAbout';
 
 import Nav from '../components/layout/Nav';
 
-import { background } from '../assets/script/animation';
+import { background, opacity } from '../assets/script/animation';
 
 const Home = () => {
     const [menuActive, setMenuActive] = useState(false);
@@ -37,12 +37,30 @@ const Home = () => {
                         <Link to="/home">Y</Link>
                     </h1>
                     <div className="burger" onClick={() => { setMenuActive(!menuActive); }}>
-                        <svg width="800px" height="800px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-                            <rect width="352" height="32" x="80" y="96" fill="var(--ci-primary-color, #000000)" className="ci-primary" />
-                            <rect width="352" height="32" x="80" y="240" fill="var(--ci-primary-color, #000000)" className="ci-primary" />
-                            <rect width="352" height="32" x="80" y="384" fill="var(--ci-primary-color, #000000)" className="ci-primary" />
-                        </svg>
+                        {!menuActive ? (
+                            <svg width="800px" height="800px" viewBox="0 0 512 512" fill="#000000">
+                                <rect width="352" height="32" x="80" y="96" fill="var(--ci-primary-color, #000000)" className="ci-primary" />
+                                <rect width="352" height="32" x="80" y="240" fill="var(--ci-primary-color, #000000)" className="ci-primary" />
+                                <rect width="352" height="32" x="80" y="384" fill="var(--ci-primary-color, #000000)" className="ci-primary" />
+                            </svg>
+                        ) : (
+                            <svg viewBox="0 -0.5 21 21" version="1.1" fill="#000000">
+                                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                                <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <defs> </defs>
+                                    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                                        <g id="Dribbble-Light-Preview" transform="translate(-419.000000, -240.000000)" fill="#000000">
+                                            <g id="icons" transform="translate(56.000000, 160.000000)">
+                                                <polygon id="close-[#1511]" points="375.0183 90 384 98.554 382.48065 100 373.5 91.446 364.5183 100 363 98.554 371.98065 90 363 81.446 364.5183 80 373.5 88.554 382.48065 80 384 81.446"></polygon>
+                                            </g>
+                                        </g>
+                                    </g>
+                                </g>
+                            </svg>
+                        )}
                     </div>
+
                 </div>
                 <div className="mainSlider__menu">
                     <AnimatePresence mode='wait'>
